ec54ce3b21f8fc55f98d90604158940e2788c61a
Dennis Silva <dev.dennissilva@gmail.com>
2023-01-20 10:13:41 +0000

    project upload

--- README.md
+++ README.md
@@ -1,111 +1,73 @@
-# Cinema Room Manager
+# Zookeeper
 
-[Cinema Room Manager](https://hyperskill.org/projects/133) provides an exciting opportunity to learn the fundamentals of programming and develop an application that helps manage a movie theater. With this application, users can book seats, track ticket revenue and gain insight into various analytics. It also allows them to take advantage of manipulating arrays, writing functions, using loops and if-statements.
+[Zookeeper](https://hyperskill.org/projects/229) provides some beginner entry level fundamentals, and approaches syntax basics, working with variables, data storage types such as arrays, and while loops.
 
 Code sample:
 
 ```
-Enter the number of rows:
-8
-Enter the number of seats in each row:
-7
-
-1. Show the seats
-2. Buy a ticket  
-3. Statistics    
-0. Exit
-1
-
-Cinema:
-  1 2 3 4 5 6 7 
-1 S S S S S S S 
-2 S S S S S S S 
-3 S S S S S S S 
-4 S S S S S S S 
-5 S S S S S S S 
-6 S S S S S S S 
-7 S S S S S S S 
-8 S S S S S S S  
-
-1. Show the seats
-2. Buy a ticket  
-3. Statistics    
-0. Exit
+Please enter the number of the habitat you would like to view:
 2
-
-Enter a row number:
-2
-Enter a seat number in that row:
-1
-Ticket price: $10
-
-1. Show the seats
-2. Buy a ticket  
-3. Statistics    
-0. Exit
-2
-
-Enter a row number:
-2
-Enter a seat number in that row:
+Switching on the camera in the deer habitat...
+   /|       |\
+`__\       //__'
+   ||      ||
+ \__`\     |'__/
+   `_\   //_'
+   _.,:---;,._
+   \_:     :_/
+     |@. .@|
+     |     |
+     ,\.-./ \
+     ;;`-'   `---__________-----.-.
+     ;;;                         \_\
+     ';;;                         |
+      ;    |                      ;
+       \   \     \        |      /
+        \_, \    /        \     |\
+          |';|  |,,,,,,,,/ \    \ \_
+          |  |  |           \   /   |
+          \  \  |           |  / \  |
+           | || |           | |   | |
+           | || |           | |   | |
+           | || |           | |   | |
+           |_||_|           |_|   |_|
+          /_//_/           /_/   /_/
+Our 'Bambi' looks hungry. Let's go to feed it!
+Please enter the number of the habitat you would like to view:
 3
-Ticket price: $10
+Switching on the camera in the goose habitat...
 
-1. Show the seats
-2. Buy a ticket
-3. Statistics
-0. Exit
-2
-
-Enter a row number:
-2
-Enter a seat number in that row:
+                                    _
+                                ,-"" "".
+                              ,'  ____  `.
+                            ,'  ,'    `.  `._
+   (`.         _..--.._   ,'  ,'        \    \
+  (`-.\    .-""        ""'   /          (  d _b
+ (`._  `-"" ,._             (            `-(   \
+ <_  `     (  <`<            \              `-._\
+  <`-       (__< <           :
+   (__        (_<_<          ;
+    `------------------------------------------
+The goose is staring intently at you... Maybe it's time to change the channel?
+Please enter the number of the habitat you would like to view:
 1
-That ticket has already been purchased!
-
-Enter a row number:
-10
-Enter a seat number in that row:
-2
-Wrong input!
-
-Enter a row number:
-3
-Enter a seat number in that row:
-4
-Ticket price: $10
-
-1. Show the seats
-2. Buy a ticket
-3. Statistics
-0. Exit
-1
-
-Cinema:
-  1 2 3 4 5 6 7
-1 S S S S S S S
-2 B S B S S S S
-3 S S S B S S S
-4 S S S S S S S
-5 S S S S S S S
-6 S S S S S S S
-7 S S S S S S S
-8 S S S S S S S 
-
-1. Show the seats
-2. Buy a ticket
-3. Statistics
-0. Exit
-3
-
-Number of purchased tickets: 3
-Percentage: 5,36%
-Current income: $30
-Total income: $560
-
-1. Show the seats
-2. Buy a ticket
-3. Statistics
-0. Exit
-0
+Switching on the camera in the lion habitat...
+                                               ,w.
+                                             ,YWMMw  ,M  ,
+                        _.---.._   __..---._.'MMMMMw,wMWmW,
+                   _.-""        '''           YP"WMMMMMMMMMb,
+                .-' __.'                   .'     MMMMW^WMMMM;
+    _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+ ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\
+,MM:.    .'.-'   .'     ;     `\    ;     `,     MMMMMMMW `"=./`-,
+WMMm__,-'.'     /      _.\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+"^MP__.-'    ,-' _.--""   `-,   ;       \  ; ;MMMMMMMMMMW^``; __|
+           /   .'            ; ;         )  )`{  \ `"^W^`,   \  :
+          /  .'             /  (       .'  /     Ww._     `.  `"
+         /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+        (--, )                `,_ / `) \/"")      ^"      `-, -;"\:
+The lion is roaring!
+Please enter the number of the habitat you would like to view:
+exit
+See you later!
 ```
--- hyperskill-Zookeeper-Java/src/main/Main.java
+++ hyperskill-Zookeeper-Java/src/main/Main.java
@@ -3,139 +3,139 @@ package src.main;
 import java.util.Scanner;
 
 public class Main {
-    private static final int PRICE_FRONT_HALF = 10;
-    private static final int PRICE_BACK_HALF = 8;
-
     public static void main(String[] args) {
+        String camel = """
+            Switching on the camera in the camel habitat...
+             ___.-''''-.
+            /___  @    |
+            ',,,,.     |         _.'''''''._
+                 '     |        /           \\
+                 |     \\    _.-'             \\
+                 |      '.-'                  '-.
+                 |                               ',
+                 |                                '',
+                  ',,-,                           ':;
+                       ',,| ;,,                 ,' ;;
+                          ! ; !'',,,',',,,,'!  ;   ;:
+                         : ;  ! !       ! ! ;  ;   :;
+                         ; ;   ! !      ! !  ; ;   ;,
+                        ; ;    ! !     ! !   ; ;
+                        ; ;    ! !    ! !     ; ;
+                       ;,,      !,!   !,!     ;,;
+                       /_I      L_I   L_I     /_I
+            Look at that! Our little camel is sunbathing!""";
+
+        String lion = """
+            Switching on the camera in the lion habitat...
+                                                           ,w.
+                                                         ,YWMMw  ,M  ,
+                                    _.---.._   __..---._.'MMMMMw,wMWmW,
+                               _.-""        '''           YP"WMMMMMMMMMb,
+                            .-' __.'                   .'     MMMMW^WMMMM;
+                _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+             ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\\
+            ,MM:.    .'.-'   .'     ;     `\\    ;     `,     MMMMMMMW `"=./`-,
+            WMMm__,-'.'     /      _.\\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+            "^MP__.-'    ,-' _.--""   `-,   ;       \\  ; ;MMMMMMMMMMW^``; __|
+                       /   .'            ; ;         )  )`{  \\ `"^W^`,   \\  :
+                      /  .'             /  (       .'  /     Ww._     `.  `"
+                     /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+                    (--, )                `,_ / `) \\/"")      ^"      `-, -;"\\:
+            The lion is roaring!""";
+
+        String deer = """
+            Switching on the camera in the deer habitat...
+               /|       |\\
+            `__\\       //__'
+               ||      ||
+             \\__`\\     |'__/
+               `_\\   //_'
+               _.,:---;,._
+               \\_:     :_/
+                 |@. .@|
+                 |     |
+                 ,\\.-./ \\
+                 ;;`-'   `---__________-----.-.
+                 ;;;                         \\_\\
+                 ';;;                         |
+                  ;    |                      ;
+                   \\   \\     \\        |      /
+                    \\_, \\    /        \\     |\\
+                      |';|  |,,,,,,,,/ \\    \\ \\_
+                      |  |  |           \\   /   |
+                      \\  \\  |           |  / \\  |
+                       | || |           | |   | |
+                       | || |           | |   | |
+                       | || |           | |   | |
+                       |_||_|           |_|   |_|
+                      /_//_/           /_/   /_/
+            Our 'Bambi' looks hungry. Let's go to feed it!""";
+
+        String goose = """
+            Switching on the camera in the goose habitat...
+            
+                                                _
+                                            ,-"" "".
+                                          ,'  ____  `.
+                                        ,'  ,'    `.  `._
+               (`.         _..--.._   ,'  ,'        \\    \\
+              (`-.\\    .-""        ""'   /          (  d _b
+             (`._  `-"" ,._             (            `-(   \\
+             <_  `     (  <`<            \\              `-._\\
+              <`-       (__< <           :
+               (__        (_<_<          ;
+                `------------------------------------------
+            The goose is staring intently at you... Maybe it's time to change the channel?""";
+
+        String bat = """
+            Switching on the camera in the bat habitat...
+            _________________               _________________
+             ~-.              \\  |\\___/|  /              .-~
+                 ~-.           \\ / o o \\ /           .-~
+                    >           \\  W  //           <
+                   /             /~---~\\             \\
+                  /_            |       |            _\\
+                     ~-.        |       |        .-~
+                        ;        \\     /        i
+                       /___      /\\   /\\      ___\\
+                            ~-. /  \\_/  \\ .-~
+                               V         V
+            This bat looks like it's doing fine.""";
+
+        String rabbit = """
+            Switching on the camera in the rabbit habitat...
+                     ,
+                    /|      __
+                   / |   ,-~ /
+                  Y :|  //  /
+                  | jj /( .^
+                  >-"~"-v"
+                 /       Y
+                jo  o    |
+               ( ~T~     j
+                >._-' _./
+               /   "~"  |
+              Y     _,  |
+             /| ;-"~ _  l
+            / l/ ,-"~    \\
+            \\//\\/      .- \\
+             Y        /    Y
+             l       I     !
+             ]\\      _\\    /"\\
+            (" ~----( ~   Y.  )
+            It looks like we will soon have more rabbits!""";
+
+        String[] animals = {camel, lion, deer, goose, bat, rabbit};
         Scanner scanner = new Scanner(System.in);
-        int rows = getNumberOfRows(scanner);
-        int seats = getNumberOfSeats(scanner);
-        int[][] cinema = new int[rows][seats];
 
-        int opt;
+        // write your code here
         do {
-            opt = showMenu(scanner);
-            switch (opt) {
-                case 1 -> printSeating(cinema);
-                case 2 -> buyTicket(scanner, cinema, rows, seats);
-                case 3 -> showStatistics(cinema, rows, seats);
-            }
-        } while (opt != 0);
-    }
+            System.out.println("Please enter the number of the habitat you would like to view:");
+            String input = scanner.nextLine();
+            if (input.equals("exit")) break;
+            System.out.println(animals[Integer.parseInt(input)]);
+        } while (true);
 
-    private static int getNumberOfRows(Scanner scanner) {
-        System.out.println("Enter the number of rows:");
-        return scanner.nextInt();
-    }
-
-    private static int getNumberOfSeats(Scanner scanner) {
-        System.out.println("Enter the number of seats in each row:");
-        return scanner.nextInt();
-    }
-
-    private static int showMenu(Scanner scanner) {
-        System.out.println();
-        System.out.println("1. Show the seats");
-        System.out.println("2. Buy a ticket");
-        System.out.println("3. Statistics");
-        System.out.println("0. Exit");
-        return scanner.nextInt();
-    }
-
-    // Print seating arrangement
-    private static void printSeating(int[][] cinema) {
-        System.out.print("\nCinema:\n  ");
-
-        int rows = cinema.length;
-        int seats = cinema[0].length;
-
-        // Print column numbers
-        for (int i = 1; i <= seats; i++) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-        // Print seating arrangement
-        for (int i = 0; i < rows; i++) {
-            System.out.print(i+1 + " ");
-            for (int j = 0; j < seats; j++) {
-                if (cinema[i][j] == 1) {
-                    System.out.print("B ");
-                } else {
-                    System.out.print("S ");
-                }
-            }
-            System.out.println();
-        }
-    }
-
-    private static void buyTicket(Scanner scanner, int[][] cinema, int rows, int seats) {
-        boolean purchased = false;
-        do {
-            // Read row and seat number
-            System.out.println();
-            System.out.println("Enter a row number:");
-            int rowNum = scanner.nextInt();
-            System.out.println("Enter a seat number in that row:");
-            int seatNum = scanner.nextInt();
-
-            if (rowNum - 1 >= rows || seatNum - 1 >= seats || rowNum <= 0 || seatNum <= 0) {
-                System.out.println("Wrong input!");
-            } else {
-                // Mark seat as bought
-                if (cinema[rowNum - 1][seatNum - 1] == 0) {
-                    cinema[rowNum - 1][seatNum - 1] = 1;
-
-                    // Calculate ticket price
-                    System.out.println("Ticket price: $" + getPrice(rows, seats, rowNum));
-                    purchased = true;
-
-                } else {
-                    System.out.println("That ticket has already been purchased!");
-                }
-            }
-        } while (!purchased);
-    }
-
-    // Calculate ticket price
-    private static int getPrice(int rows, int seats, int selectedRow) {
-        if (rows * seats <= 60) {
-            return PRICE_FRONT_HALF;
-        } else {
-            return (selectedRow <= rows / 2) ? PRICE_FRONT_HALF : PRICE_BACK_HALF;
-        }
-    }
-
-    private static int currentIncome(int[][] cinema, int rows, int seats) {
-        int currentIncome = 0;
-        for (int i = 0; i < cinema.length; i++) {
-            for (int j = 0; j < cinema[0].length; j++) {
-                if (cinema[i][j] == 1) {
-                    currentIncome += getPrice(rows, seats, i+1);
-                }
-            }
-        }
-        return currentIncome;
-    }
-
-    private static void showStatistics(int[][] cinema, int rows, int seats) {
-        int totalSeats = rows * seats;
-        int totalIncome = totalSeats <= 60 ? totalSeats * PRICE_FRONT_HALF : rows / 2 * seats * PRICE_FRONT_HALF
-                + (rows - rows / 2) * seats * PRICE_BACK_HALF;
-        int occupiedSeats = 0;
-
-        for (int[] ints : cinema) {
-            for (int j = 0; j < cinema[0].length; j++) {
-                if (ints[j] == 1) {
-                    occupiedSeats++;
-                }
-            }
-        }
-
-        System.out.println();
-        System.out.println("Number of purchased tickets: " + occupiedSeats);
-        System.out.printf("Percentage: %.2f%%%n", (float)occupiedSeats * 100 / totalSeats);
-        System.out.println("Current income: $" + currentIncome(cinema, rows, seats));
-        System.out.println("Total income: $" + totalIncome);
+        System.out.println("See you later!");
     }
 }
